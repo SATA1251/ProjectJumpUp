ac458d1bb84c387c7117fc1fea515e9db9e7f9ea
Sunggeun <goldpig622@naver.com>
2025-01-17 22:00:25 +0900

    이펙트 파티클 에셋 추가, 드래그 점프 기능 임시추가, 필수 오브젝트 생성 완료

---
+++ ProjectJumpUp/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector2 dragStartPosition; // 드래그 시작 위치
+    private Vector2 currentDragPosition; // 드래그 종료 위치
+    Rigidbody2D rb;
+    private LineRenderer lineRenderer;
+
+    [SerializeField]
+    private float throwForce = 10f;
+
+
+    private bool isDragging = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2; // 시작점과 끝점 표시
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerJump();
+    }
+
+    void PlayerJump()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                dragStartPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                lineRenderer.enabled = true;
+            }
+           else if (touch.phase == TouchPhase.Moved)
+            {
+                currentDragPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+                Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
+  
+
+                lineRenderer.SetPosition(0, position2D - dragStartPosition); // 문제생기면 수정
+                lineRenderer.SetPosition(1, dragStartPosition);
+
+                Vector2 dragVector = dragStartPosition - currentDragPosition;
+                float dragDistance = dragVector.magnitude;
+
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Vector2 dragVector = dragStartPosition - currentDragPosition;
+                ThrowObject(dragVector);
+
+                lineRenderer.enabled = false;
+            }
+        }
+    }
+
+
+    private void ThrowObject(Vector2 direction)  // 일정 힘으로만 날림
+    {
+        rb.velocity = Vector2.zero;
+
+        float dragDistance = direction.magnitude; // 벡터의 길이
+        Vector2 throwForceVector = direction.normalized * dragDistance * throwForce;
+
+        //힘 적용
+        rb.AddForce(throwForceVector, ForceMode2D.Impulse);
+    }
+}
